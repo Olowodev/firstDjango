cea3b499a0426ca2882ab9916711f3f41b4c93ca
Adebayo Olowofoyeku <adebayoolowofoyeku@gmail.com>
2022-07-14 18:34:39 +0100

    update

--- firstDjango/settings.py
+++ firstDjango/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*(1szp7r&bn%sl!5t+q941t#62zt#*2ghuvn3c7ij4jj^&!lq1'
+SECRET_KEY = config("SECRET_KEY", default='')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
